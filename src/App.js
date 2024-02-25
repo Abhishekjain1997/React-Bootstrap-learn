4508e48271ffcf9ce86f830a2a16095f4f363815
Abhishekjain1997 <abhishekjain2589@gmail.com>
2024-02-25 11:34:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Button,Alert,Accordion,Badge} from "react-bootstrap"
 
-function App() {
+const App=()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+     <h2>Components</h2>
+     <Button variant="primary">Primary</Button>{' '}
+     <Button variant="secondary">Secondary</Button>{' '}
+     <Button variant="success">Success</Button>{' '}
+     <Button variant="warning">Warning</Button>{' '}
+     <Button variant="danger">Danger</Button>{' '}
+     <Button variant="info">Info</Button>{' '}
+     <Button variant="light">Light</Button>{' '}
+     <Button variant="dark">Dark</Button>
+     <Button variant="link">Link</Button>
+
+     
+      <Alert variant='success' className='mt-3'>
+      This is a 
+       <Alert.Link href="#" >an example link</Alert.Link>. Give it a click if your like.
+      </Alert>
+      <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    <div>
+    <h1>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h1>
+    <h2>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h2>
+    <h3>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h3>
+    <h4>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h4>
+    <h5>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h5>
+    <h6>
+      Example heading <Badge bg="secondary">New</Badge>
+    </h6>
+  </div>
+
+         
+  </> 
   );
 }
 
